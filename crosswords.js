04f9c18c168d9867c8d926d2c24ecb274a510ee9
abrudz <a.brudz@gmail.com>
2019-11-19 21:44:25 +0000

    remove filler, uppercase, overhaul js, indicate missing features, allow lettered clues

--- crosswords.js
+++ crosswords.js
@@ -1,7 +1,17 @@
-i=t=0;
-I=s=>{t+=s
- x.innerHTML=x.getAttribute("data-c").replace(new RegExp(".{"+s+"}","g"),r=>"<tr>"+[...r].map(c=>"<td class="+c+"><input id=c"+(i++)+" onfocus=this.select() onkeyup=this.select() onclick=this.select() onchange=this.select() value=' ' maxlength="+(c==="b"?"0' readonly":"1")+" /></td>").join("")+"</tr>");
- (q=document.querySelectorAll(".n input,.e input"))[0].focus()}
-C=_=>{if((new RegExp(x.getAttribute("data-a"))).test([...q].map(i=>i.value).join("")))alert("That's correct!")}
-document.addEventListener("keydown",e=>
-document.getElementById("c"+((i=+document.activeElement.id.slice(1))+((k=e.key)=="ArrowLeft"?(t*!(i%t))-1:k=="ArrowRight"?1-t*(!((i+1)%t)):k=="ArrowUp"?-t:k=="ArrowDown"?t:k=="Home"?-i%t:k=="End"?t-i%t-1:k=="PageUp"?i%t-i:k=="PageDown"?i%t-i-t:0)+t*t)%(t*t)).focus())
+D=document;
+$=q=>[...D.querySelectorAll(q)];
+I=t=>{n=0;
+ x.innerHTML=x.getAttribute("data-c").replace(new RegExp(".{"+t+"}","gi"),r=>"<tr>"+[...r].map(c=>"<td class="+c+"><input id=c"+(n++)+" onfocus=this.select() onkeyup=this.select() onclick=this.select() onchange=this.select() oninput=;C() value=' ' maxlength="+(c==="b"?"0' readonly":"1")+" /></td>").join("")+"</tr>");
+ D.addEventListener("keydown",e=>D.getElementById("c"+((i=+D.activeElement.id.slice(1))+((k=e.key)
+  =="ArrowLeft"?(t*!(i%t))-1:
+ k=="ArrowRight"?1-t*(!((i+1)%t)):
+ k=="ArrowUp"?-t:
+ k=="ArrowDown"?t:
+ k=="Home"?-i%t:
+ k=="End"?t-i%t-1:
+ k=="PageUp"?i%t-i:
+ k=="PageDown"?i%t-i-t:
+ 0)+t*t)%(t*t)).focus());
+ $("[data-l]").map(e=>{e.value=e.getAttribute("data-l").charCodeAt()-96;e.classList.add("l")});
+ (q=$(".n input,.e input"))[0].focus()}
+C=_=>{if((new RegExp(x.getAttribute("data-a"))).test(q.map(i=>i.value).join("")))alert("That's correct!")}
